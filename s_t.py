86b87742324f4cd00f2f946acf03d19a9e645983
Jjlopezp2 <juanjoseabcd.jj@gmail.com>
2026-08-27 08:04:07 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -96,7 +96,7 @@ if result:
     
     out_lang = st.selectbox(
         "Selecciona el lenguaje de salida",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés"),
+        ("Inglés", "Islandés", "Norwegian", "Cherokee", "Mandarín", "Japonés"),
     )
     if out_lang == "Inglés":
         output_language = "en"
